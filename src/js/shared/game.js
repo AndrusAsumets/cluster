f326cefdd59ac13ca00f29980ef318d27bda6632
Andrus Asumets <andrus.asumets@gmail.com>
2017-09-08 22:13:55 +0000

    scoring

--- src/js/shared/game.js
+++ src/js/shared/game.js
@@ -548,13 +548,14 @@ export function game() {
 		for (var p = 0; p < elements.length; p++) {
 			var element = player.elements[p]
 			
-			if (
+			if (element.inactive) { continue }
+			else if (
 				!element.path ||
 				!element.path[1] ||
 				!element.path[1].length
 			) {
-				players[player.id].elements.splice(p, 1)
 				players[player.id].energy = players[player.id].energy - 1
+				players[player.id].elements[p].inactive = true
 			} else {
 				players[player.id].elements[p].path.shift()
 			}
