8ea22067a56c9b4577cbd4097fba008e8691f449
Andrus Asumets <andrus.asumets@gmail.com>
2017-10-20 13:17:35 +0000

    sentry.io

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-Raven.config('https://7ec87dafe6184836b41f26bc7604ab09:1039e6aed3dd41d68d49bdd27af3c646@sentry.io/204372')
+Raven.config('https://7ec87dafe6184836b41f26bc7604ab09@sentry.io/204372')
 require('./src/css/app.css')
 
 import { game } from './src/js/shared/game'
